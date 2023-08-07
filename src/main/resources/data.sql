ef7fdfa889ed3704cc5253d067e906dac770feb2
Alexandr <jwchaganav@gmail.com>
2023-08-07 13:18:04 +0400

    account

---
+++ src/main/resources/data.sql
@@ -0,0 +1,2 @@
+insert into account values(null,'Jane', 1000);
+insert into account values(null,'John', 1000);
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -3,3 +3,8 @@ CREATE TABLE IF NOT EXISTS purchase(
     product varchar(50) NOT NULL,
     price double NOT NULL
 );
+create table if not exists account(
+    id int auto-increment primary key not null,
+    name varchar(50) not null,
+    amount double not null
+);
