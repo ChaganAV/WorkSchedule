07a0b3789f8d8aae7c445692f8751c00efd4b8df
Alexandr <jwchaganav@gmail.com>
2023-08-14 16:11:08 +0400

    layout

--- src/main/java/com/example/WorkSchedule/config/ProjectConfig.java
+++ src/main/java/com/example/WorkSchedule/config/ProjectConfig.java
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ProjectConfig {
-    @Bean
+    //@Bean
     public LayoutDialect layoutDialect(){
         return new LayoutDialect();
     }
